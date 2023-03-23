7e39f1cbeb766bb3ad4239a98be4608372e6fb3a
Ronnie <43523873+BlissfulCoda@users.noreply.github.com>
2023-03-23 00:22:58 +0000

    feat(server): add dotenv file for API key security

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,24 @@
-import express from "express";
+import express, { Express, Request, Response} from "express";
+import dotenv from 'dotenv'
+import cors from 'cors'
 
-const server = express();
+
+// Middleware
+dotenv.config()
+
+const server: Express = express();
 
 const PORT = 8000;
 
-server.get("/", (req, res) => {
-  res.send(`Heloooo from the backend`);
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+
+// ENABLE CORS
+server.use(cors());
+
+server.get("/github", (req: Request, res: Response) => {
+  res.send(`Github data from backend`);
 });
+
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
