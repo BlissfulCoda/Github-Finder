f3985b8640a1a6d1c065c741fb466831440e5d3e
Ronnie <43523873+BlissfulCoda@users.noreply.github.com>
2023-03-27 14:56:14 +0100

    fix(server): clean minor bugs

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,13 @@
-import express, { Express} from "express";
-import dotenv from 'dotenv'
-import cors from 'cors'
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import mongoose from "mongoose";
 // Middleware
-dotenv.config()
+dotenv.config();
 
 const server: Express = express();
 
-const PORT = 5000;
+const PORT = 8000;
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
@@ -17,9 +17,6 @@ server.use(cors());
 
 server.use("/github", require("./routes/githubRoutes"));
 server.use("/feedback", require("./routes/feedbackRoutes"));
-
-
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
 mongoose.set("strictQuery", false);
